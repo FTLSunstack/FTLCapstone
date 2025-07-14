464323ce3f19df2a10c6bb634f781b251531d595
Heiryn Hernandez Rojas <heiryn13@gmail.com>
2025-07-14 14:07:32 -0700

    set up user Auth on the backend with JWT Tokens

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,26 @@
-const express = require('express')
+const express = require('express');
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
 
-const app = express()
-const PORT = 3000
+app.use(express.json());
+app.use(cookieParser());
+
+const authRouter = require("./routes/authRoutes");
+app.use("/auth", authRouter);
 
 app.get('/', (req, res) => {
   res.send('This is Codifica!')
 })
+
+app.use(cors({
+  // front end port
+  origin: 'http://localhost:5173',
+  // this allows the cookies to be sent/used
+  credentials: true
+}));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
