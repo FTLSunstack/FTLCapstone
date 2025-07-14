9cb64b511c5033ae84ab4b028aa0b3f0075a6a91
Luimoe05 <lmoreno00528@gmail.com>
2025-07-14 16:20:17 -0700

    quick edit

--- Frontend/src/Components/App/App.jsx
+++ Frontend/src/Components/App/App.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import reactLogo from "../../assets/react.svg";
 import viteLogo from "/vite.svg";
 import "../../tailwind.css";
-
+import IDEPage from "../Pages/IDE/IDEPage/IDEPage";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <h1 className="text-red-500">HELLO</h1>
+        <IDEPage />
       </div>
     </>
   );
