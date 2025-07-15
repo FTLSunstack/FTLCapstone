3c0980276f9dbf1fc0daf2e9dbc8b666acab67bd
Heiryn Hernandez Rojas <heiryn13@gmail.com>
2025-07-15 14:59:19 -0700

    Setting Up Page Routes and Adding functionality to Nav Bar

--- Frontend/src/Components/App/App.jsx
+++ Frontend/src/Components/App/App.jsx
@@ -5,18 +5,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../../tailwind.css";
 import HomePage from "../Pages/Home/HomePage/HomePage.jsx";
 import IDEPage from "../Pages/IDE/IDEPage/IDEPage";
-
+import LoginPage from "../Pages/Auth/LoginPage.jsx";
 import GlossaryPage from "../Pages/Glossary/GlossaryPage/GlossaryPage.jsx";
 
 function App() {
   return (
-    <>
-      <div>
-        {/* <IDEPage /> */}
-        <HomePage />
-        <GlossaryPage />
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/glossary" element={<GlossaryPage />} />
+        <Route path="/ide" element={<IDEPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- Frontend/src/Components/Pages/Home/HomeNavBar/HomeNavBar.jsx
+++ Frontend/src/Components/Pages/Home/HomeNavBar/HomeNavBar.jsx
@@ -1,8 +1,19 @@
 import "../../../../tailwind.css";
+import { useNavigate } from "react-router-dom";
 
 import React from "react";
 
 export default function HomeNavBar() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
+  const handleSignUp = () => {
+    navigate("/signup");
+  };
+
   return (
     <>
       <div className="bg-white p-5 flex flex-row justify-between items-center border-b border-black">
@@ -29,10 +40,10 @@ export default function HomeNavBar() {
         </div>
 
         <div className="flex flex-row space-x-5">
-          <button className="px-5 py-2 outline outline-violet-600 text-violet-600 rounded-md hover:bg-violet-600 hover:text-white hover:cursor-pointer transition ease-in-out">
+          <button onClick={handleLogin} className="px-5 py-2 outline outline-violet-600 text-violet-600 rounded-md hover:bg-violet-600 hover:text-white hover:cursor-pointer transition ease-in-out">
             Sign In
           </button>
-          <button className="bg-red-400 px-5 py-2 bg-violet-500 text-white rounded-md outline outline-white  hover:bg-violet-600 hover:cursor-pointer transition ease-in-out">
+          <button onClick={handleSignUp} className="bg-red-400 px-5 py-2 bg-violet-500 text-white rounded-md outline outline-white  hover:bg-violet-600 hover:cursor-pointer transition ease-in-out">
             Sign Up
           </button>
         </div>
