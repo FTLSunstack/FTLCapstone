ee161c6aea7477a33b89b3e87a081d6da9939a52
Heiryn Hernandez Rojas <heiryn13@gmail.com>
2025-07-15 16:29:44 -0700

    set up login and signup front end pages

---
+++ Frontend/src/Components/Pages/Auth/LoginForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../../../tailwind.css";
+import LoginInput from "./LoginInput";
+
+
+function LoginForm() {
+    const navigate = useNavigate();
+    const handleBack = () => {
+        navigate("/");
+    };
+    const handleSignUp = ()=> {
+        navigate("/signup");
+    };
+    
+    return (
+        <div className="LoginForm bg-white bg-center flex flex-col items-center w-100 h-140 rounded-md "> 
+            <div className="Title Header align-center">
+                <div className="Title mt-10 mb-4 text-center text-violet-600 font-bold text-5xl">Codifica</div>
+                <div className="Caption mb-6 text-center text-black font-bold text-xl">Welcome Back!</div>
+            </div>
+            <LoginInput></LoginInput>
+            <container className= "flex flex-col items-left">
+                <p className="mt-4 ml-1 text-center text-indigo-600 hover:text-purple-800 transition ease-in-out hover:cursor-pointer"> Forgot password?</p>
+                <button onClick={handleBack} className="mt-2 mb-4 py-2 px-30 text-center font-semibold rounded-lg shadow-md text-white bg-indigo-700 hover:bg-blue-800 cursor-pointer"> Sign In</button>
+            </container>
+            <container className= "flex flex-row">
+                <p className="text-xs text-gray-500"> Don't have an account? </p>
+                <p onClick={handleSignUp} className="ml-3 text-xs text-gray-500 underline hover:text-indigo-500 hover:cursor-pointer"> Sign up for free</p>
+            </container>
+        </div>
+    );
+}
+
+export default LoginForm;
--- Frontend/src/Components/Pages/Auth/SignUpPage.jsx
+++ Frontend/src/Components/Pages/Auth/SignUpPage.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import "../../../tailwind.css";
-import LoginForm from "./LoginForm";
+import { useNavigate } from "react-router-dom";
+import SignUpForm from "./SignUpForm";
 
 
-function LoginPage() {
+function SignUpPage() {
     const navigate = useNavigate();
 
     const handleBack = () => {
@@ -13,13 +13,14 @@ function LoginPage() {
 
     return (
         <div className="opacity-90 min-h-screen h-14 bg-gradient-to-r from-violet-600 to-blue-800">
-            <button onClick={handleBack} className="mt-8 ml-10 mb-8 py-2 px-4 font-semibold rounded-lg shadow-md text-black bg-white hover:bg-white hover:text-black "> Back Home</button>
+            <button onClick={handleBack} className="mt-8 ml-10 mb-4 py-2 px-4 font-semibold rounded-lg shadow-md text-black bg-white hover:bg-blue-800 "> Back Home</button>
             <div className="flex flex-col items-center justify-center w-screen">
-            <LoginForm></LoginForm>
+            <SignUpForm></SignUpForm>
             </div>
+            <h1>Sign Page woo hoo</h1> 
             <h1>Footer goes here</h1> 
         </div>
     );
 }
 
-export default LoginPage;
+export default SignUpPage;
