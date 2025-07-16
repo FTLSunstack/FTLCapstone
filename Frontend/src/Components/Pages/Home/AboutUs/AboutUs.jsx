1413f6f3840c06e3a928d7f92ea11c395bded8b2
Joanna Echeverri Porras <jecheverriporras@jecheve-ltmkm4y.internal.salesforce.com>
2025-07-16 10:44:50 -0700

    working language toggle on entire home page

--- Frontend/src/Components/Pages/Home/AboutUs/AboutUs.jsx
+++ Frontend/src/Components/Pages/Home/AboutUs/AboutUs.jsx
@@ -3,38 +3,75 @@ import "../../../../tailwind.css";
 import CreatorCard from "../CreatorCard/CreatorCard.jsx";
 import Mission from "../Mission/Mission.jsx";
 
-function AboutUs() {
+function AboutUs({ language }) {
   return (
     <>
-      <Mission />
-      <div className="flex flex-col items-center justify-center w-screen bg-gray-50 p-20">
-        <h1 className="text-black text-4xl font-bold pb-8 text-center">
-          Conoce a Nuestro Equipo
-        </h1>
-        <h2 className="text-gray-500 text-xl text-center px-20">
-          Somos un equipo apasionado de desarrolladores y educadores
-          comprometidos con hacer que la programación sea accesible para todos.
-          Creemos en el poder del aprendizaje inclusivo y en la importancia de
-          eliminar las barreras lingüísticas en la educación tecnológica.
-        </h2>
-        <div className="flex flex-row justify-center mt-10">
-          <CreatorCard
-            name="Joanna Echeverri Porras"
-            description="Desarrolladora Full Stack"
-            img=""
-          />
-          <CreatorCard
-            name="Luis-Angel Moreno"
-            description="Desarrollador Full Stack"
-            img=""
-          />
-          <CreatorCard
-            name="Heiryn Hernandez Rojas"
-            description="Desarrolladora Full Stack"
-            img=""
-          />
+      {language === "Español" ? (
+        <div>
+          <Mission language={language} />
+          <div className="flex flex-col items-center justify-center w-screen bg-gray-50 p-20">
+            <h1 className="text-black text-4xl font-bold pb-8 text-center">
+              Conoce a Nuestro Equipo
+            </h1>
+            <h2 className="text-gray-500 text-xl text-center px-20">
+              Somos un equipo apasionado de desarrolladores y educadores
+              comprometidos con hacer que la programación sea accesible para
+              todos. Creemos en el poder del aprendizaje inclusivo y en la
+              importancia de eliminar las barreras lingüísticas en la educación
+              tecnológica.
+            </h2>
+            <div className="flex flex-row justify-center mt-10">
+              <CreatorCard
+                name="Joanna Echeverri Porras"
+                description="Desarrolladora Full Stack"
+                img=""
+              />
+              <CreatorCard
+                name="Luis-Angel Moreno"
+                description="Desarrollador Full Stack"
+                img=""
+              />
+              <CreatorCard
+                name="Heiryn Hernandez Rojas"
+                description="Desarrolladora Full Stack"
+                img=""
+              />
+            </div>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div>
+          <Mission language={language} />
+          <div className="flex flex-col items-center justify-center w-screen bg-gray-50 p-20">
+            <h1 className="text-black text-4xl font-bold pb-8 text-center">
+              Meet Our Team
+            </h1>
+            <h2 className="text-gray-500 text-xl text-center px-20">
+              We are a passionate team of developers and educators committed to
+              making programming accessible for everyone. We believe in the
+              power of inclusive learning and the importance of breaking down
+              language barriers in tech education.
+            </h2>
+            <div className="flex flex-row justify-center mt-10">
+              <CreatorCard
+                name="Joanna Echeverri Porras"
+                description="Full Stack Developer"
+                img=""
+              />
+              <CreatorCard
+                name="Luis-Angel Moreno"
+                description="Full Stack Developer"
+                img=""
+              />
+              <CreatorCard
+                name="Heiryn Hernandez Rojas"
+                description="Full Stack Developer"
+                img=""
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
--- Frontend/src/Components/Pages/Home/HomeNavBar/HomeNavBar.jsx
+++ Frontend/src/Components/Pages/Home/HomeNavBar/HomeNavBar.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import React from "react";
 
-export default function HomeNavBar() {
+export default function HomeNavBar({ language, setLanguage }) {
   const navigate = useNavigate();
 
   const handleLogin = () => {
@@ -14,40 +14,105 @@ export default function HomeNavBar() {
     navigate("/signup");
   };
 
+  const handleChangeLanguage = () => {
+    if (language === "English") {
+      setLanguage("Español");
+    } else {
+      setLanguage("English");
+    }
+  };
+
   return (
     <>
-      <div className="bg-white p-5 flex flex-row justify-between items-center border-b border-black">
-        <h1 className="text-violet-600 font-bold text-3xl hover:cursor-pointer hover:opacity-70 transition ease-in-out">
-          Codifica
-        </h1>
+      {language === "Español" ? (
+        <div className="bg-white p-5 flex flex-row justify-between items-center shadow-lg">
+          <h1 className="text-violet-600 font-bold text-3xl hover:cursor-pointer hover:opacity-70 transition ease-in-out">
+            Codifica
+          </h1>
 
-        <div className="flex flex-row gap-10">
-          <a href="#" className="text-gray-500 hover:text-black">
-            Features
-          </a>
-          <a
-            href="#"
-            className="text-gray-500 hover:text-black transition ease-in-out"
-          >
-            About Us
-          </a>
-          <a
-            href="#"
-            className="text-gray-500 hover:text-black transition ease-in-out"
-          >
-            Get Started
-          </a>
+          <div className="flex flex-row gap-10">
+            <a href="#" className="text-gray-500 hover:text-black">
+              Características
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-black transition ease-in-out"
+            >
+              Sobre Nosotros
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-black transition ease-in-out"
+            >
+              Comenzar
+            </a>
+          </div>
+          <div className="flex flex-row space-x-5">
+            <button
+              onClick={handleChangeLanguage}
+              className="bg-violet-500 px-5 py-2 rounded-md hover:cursor-pointer w-24 text-white hover:bg-violet-700 transition ease-in-out"
+            >
+              {language}
+            </button>
+            <button
+              onClick={handleLogin}
+              className="px-5 py-2 outline outline-violet-600 text-violet-600 rounded-md hover:bg-violet-600 hover:text-white hover:cursor-pointer transition ease-in-out"
+            >
+              Iniciar Sesión
+            </button>
+            <button
+              onClick={handleSignUp}
+              className="bg-red-400 px-5 py-2 bg-violet-500 text-white rounded-md outline outline-white  hover:bg-violet-600 hover:cursor-pointer transition ease-in-out"
+            >
+              Registrarse
+            </button>
+          </div>
         </div>
+      ) : (
+        <div className="bg-white p-5 flex flex-row justify-between items-center border-b border-black">
+          <h1 className="text-violet-600 font-bold text-3xl hover:cursor-pointer hover:opacity-70 transition ease-in-out">
+            Codifica
+          </h1>
 
-        <div className="flex flex-row space-x-5">
-          <button onClick={handleLogin} className="px-5 py-2 outline outline-violet-600 text-violet-600 rounded-md hover:bg-violet-600 hover:text-white hover:cursor-pointer transition ease-in-out">
-            Sign In
-          </button>
-          <button onClick={handleSignUp} className="bg-red-400 px-5 py-2 bg-violet-500 text-white rounded-md outline outline-white  hover:bg-violet-600 hover:cursor-pointer transition ease-in-out">
-            Sign Up
-          </button>
+          <div className="flex flex-row gap-10">
+            <a href="#" className="text-gray-500 hover:text-black">
+              Features
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-black transition ease-in-out"
+            >
+              About Us
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-black transition ease-in-out"
+            >
+              Get Started
+            </a>
+          </div>
+          <div className="flex flex-row space-x-5">
+            <button
+              onClick={handleChangeLanguage}
+              className="bg-violet-500 px-5 py-2 rounded-md hover:cursor-pointer w-24 text-white hover:bg-violet-700 transition ease-in-out"
+            >
+              {language}
+            </button>
+            <button
+              onClick={handleLogin}
+              className="px-5 py-2 outline outline-violet-600 text-violet-600 rounded-md hover:bg-violet-600 hover:text-white hover:cursor-pointer transition ease-in-out"
+            >
+              Sign In
+            </button>
+            <button
+              onClick={handleSignUp}
+              className="bg-red-400 px-5 py-2 bg-violet-500 text-white rounded-md outline outline-white  hover:bg-violet-600 hover:cursor-pointer transition ease-in-out"
+            >
+              Sign Up
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
