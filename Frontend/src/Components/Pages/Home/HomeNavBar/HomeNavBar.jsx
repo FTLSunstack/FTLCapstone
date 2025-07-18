1d90aaff45b9634fa5e7b8b30e2af1f584cd9d39
Luimoe05 <lmoreno00528@gmail.com>
2025-07-18 13:20:43 -0700

    fixed up navbar and added conditional rendering when logged in

--- Frontend/src/Components/Pages/Home/HomeNavBar/HomeNavBar.jsx
+++ Frontend/src/Components/Pages/Home/HomeNavBar/HomeNavBar.jsx
@@ -1,7 +1,8 @@
 import "../../../../tailwind.css";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../../../Context/AuthContext.jsx";
 
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function HomeNavBar({
   language,
@@ -11,6 +12,12 @@ export default function HomeNavBar({
   onScrollToGetStarted,
 }) {
   const navigate = useNavigate();
+  const { user, loguout } = useAuth();
+
+  useEffect(() => {
+    console.log(user);
+    console.log(loguout);
+  });
 
   const handleLogin = () => {
     navigate("/login");
@@ -36,26 +43,49 @@ export default function HomeNavBar({
             Codifica
           </h1>
 
-          <div className="flex flex-row gap-10 ml-60">
-            <button
-              onClick={onScrollToFeatures}
-              className="text-gray-500 hover:text-black"
-            >
-              Características
-            </button>
-            <button
-              onClick={onScrollToAboutUs}
-              className="text-gray-500 hover:text-black transition ease-in-out"
-            >
-              Sobre Nosotros
-            </button>
-            <button
-              onClick={onScrollToGetStarted}
-              className="text-gray-500 hover:text-black transition ease-in-out"
-            >
-              Comenzar
-            </button>
-          </div>
+          {user ? (
+            <div className="flex flex-row gap-10">
+              <button
+                onClick={onScrollToFeatures}
+                className="text-gray-500 hover:text-black transition ease-in-out cursor-pointer"
+              >
+                Características
+              </button>
+              <button
+                onClick={onScrollToAboutUs}
+                className="text-gray-500 hover:text-black transition ease-in-out cursor-pointer"
+              >
+                Sobre Nosotros
+              </button>
+              <button
+                onClick={onScrollToGetStarted}
+                className="text-gray-500 hover:text-black transition ease-in-out cursor-pointer"
+              >
+                Comenzar
+              </button>
+            </div>
+          ) : (
+            <div className="flex flex-row gap-10 ml-60">
+              <button
+                onClick={onScrollToFeatures}
+                className="text-gray-500 hover:text-black transition ease-in-out cursor-pointer"
+              >
+                Características
+              </button>
+              <button
+                onClick={onScrollToAboutUs}
+                className="text-gray-500 hover:text-black transition ease-in-out cursor-pointer"
+              >
+                Sobre Nosotros
+              </button>
+              <button
+                onClick={onScrollToGetStarted}
+                className="text-gray-500 hover:text-black transition ease-in-out cursor-pointer"
+              >
+                Comenzar
+              </button>
+            </div>
+          )}
           <div className="flex flex-row space-x-5">
             <button
               onClick={handleChangeLanguage}
@@ -63,18 +93,31 @@ export default function HomeNavBar({
             >
               {language}
             </button>
-            <button
-              onClick={handleLogin}
-              className="px-5 py-2 outline outline-violet-600 text-violet-600 rounded-md hover:bg-violet-600 hover:text-white hover:cursor-pointer transition ease-in-out"
-            >
-              Iniciar Sesión
-            </button>
-            <button
-              onClick={handleSignUp}
-              className="bg-red-400 px-5 py-2 bg-violet-500 text-white rounded-md outline outline-white  hover:bg-violet-600 hover:cursor-pointer transition ease-in-out"
-            >
-              Registrarse
-            </button>
+
+            {user ? (
+              <span>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png"
+                  alt="pfp"
+                  className="w-10 cursor-pointer"
+                />
+              </span>
+            ) : (
+              <div className="space-x-5">
+                <button
+                  onClick={handleLogin}
+                  className="px-5 py-2 outline outline-violet-600 text-violet-600 rounded-md hover:bg-violet-600 hover:text-white hover:cursor-pointer transition ease-in-out"
+                >
+                  Iniciar Sesión
+                </button>
+                <button
+                  onClick={handleSignUp}
+                  className="bg-red-400 px-5 py-2 bg-violet-500 text-white rounded-md outline outline-white  hover:bg-violet-600 hover:cursor-pointer transition ease-in-out"
+                >
+                  Registrarse
+                </button>
+              </div>
+            )}
           </div>
         </div>
       ) : (
@@ -83,26 +126,49 @@ export default function HomeNavBar({
             Codifica
           </h1>
 
-          <div className="flex flex-row gap-10">
-            <button
-              onClick={onScrollToFeatures}
-              className="text-gray-500 hover:text-black"
-            >
-              Features
-            </button>
-            <button
-              onClick={onScrollToAboutUs}
-              className="text-gray-500 hover:text-black transition ease-in-out"
-            >
-              About Us
-            </button>
-            <button
-              onClick={onScrollToGetStarted}
-              className="text-gray-500 hover:text-black transition ease-in-out"
-            >
-              Get Started
-            </button>
-          </div>
+          {user ? (
+            <div className="flex flex-row gap-10">
+              <button
+                onClick={onScrollToFeatures}
+                className="text-gray-500 hover:text-black"
+              >
+                Features
+              </button>
+              <button
+                onClick={onScrollToAboutUs}
+                className="text-gray-500 hover:text-black transition ease-in-out"
+              >
+                About Us
+              </button>
+              <button
+                onClick={onScrollToGetStarted}
+                className="text-gray-500 hover:text-black transition ease-in-out"
+              >
+                Get Started
+              </button>
+            </div>
+          ) : (
+            <div className="flex flex-row gap-10 ml-50">
+              <button
+                onClick={onScrollToFeatures}
+                className="text-gray-500 hover:text-black"
+              >
+                Features
+              </button>
+              <button
+                onClick={onScrollToAboutUs}
+                className="text-gray-500 hover:text-black transition ease-in-out"
+              >
+                About Us
+              </button>
+              <button
+                onClick={onScrollToGetStarted}
+                className="text-gray-500 hover:text-black transition ease-in-out"
+              >
+                Get Started
+              </button>
+            </div>
+          )}
           <div className="flex flex-row space-x-5">
             <button
               onClick={handleChangeLanguage}
@@ -110,18 +176,30 @@ export default function HomeNavBar({
             >
               {language}
             </button>
-            <button
-              onClick={handleLogin}
-              className="px-5 py-2 outline outline-violet-600 text-violet-600 rounded-md hover:bg-violet-600 hover:text-white hover:cursor-pointer transition ease-in-out"
-            >
-              Sign In
-            </button>
-            <button
-              onClick={handleSignUp}
-              className="bg-red-400 px-5 py-2 bg-violet-500 text-white rounded-md outline outline-white  hover:bg-violet-600 hover:cursor-pointer transition ease-in-out"
-            >
-              Sign Up
-            </button>
+            {user ? (
+              <span>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png"
+                  alt="pfp"
+                  className="w-10 cursor-pointer"
+                />
+              </span>
+            ) : (
+              <div className="space-x-5">
+                <button
+                  onClick={handleLogin}
+                  className="px-5 py-2 outline outline-violet-600 text-violet-600 rounded-md hover:bg-violet-600 hover:text-white hover:cursor-pointer transition ease-in-out"
+                >
+                  Sign In
+                </button>
+                <button
+                  onClick={handleSignUp}
+                  className="bg-red-400 px-5 py-2 bg-violet-500 text-white rounded-md outline outline-white  hover:bg-violet-600 hover:cursor-pointer transition ease-in-out"
+                >
+                  Sign Up
+                </button>
+              </div>
+            )}
           </div>
         </div>
       )}
