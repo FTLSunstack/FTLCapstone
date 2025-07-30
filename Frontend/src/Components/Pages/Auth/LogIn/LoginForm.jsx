7d32509fb6ef6358f96ad0d3116184176eed43dd
Heiryn Hernandez Rojas <heiryn13@gmail.com>
2025-07-30 16:24:22 -0700

    added a use effect that takes in the language prop from the homepage and then changes the language of the login page accordingly

--- Frontend/src/Components/Pages/Auth/LogIn/LoginForm.jsx
+++ Frontend/src/Components/Pages/Auth/LogIn/LoginForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../../../../tailwind.css";
@@ -11,14 +11,39 @@ import {
   NotifError,
 } from "../../../Common/ToastNotifs/ToastNotifs";
 
-function LoginForm() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [modalOpen, setModal] = useState(false);
-  const { setUser } = useAuth();
+function LoginForm({language}) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [modalOpen, setModal] = useState(false);
+    const { setUser } = useAuth();
 
-  const navigate = useNavigate();
+    const [forgotPasswordTxt, setforgotPasswordTxt] = useState( "¿Olvidó su contraseña?" );
+    const [loginButtonTxt, setLoginButtonTxt] = useState( "Iniciar Sesión" );
+    const [WelcomeTxt, setWelcomeTxt] = useState("¡Bienvenido de vuelta!");
+    const [AccountTxt, setAccountTxt] = useState("¿No tiene una cuenta?");
+    const [signupTxt, setSignupTxt] = useState( "Regístrese Gratis Aquí");
+
+    useEffect(() => {
+        // only if language changes
+        if (language === "Español") {
+            setforgotPasswordTxt("¿Olvidó su contraseña?");
+            setLoginButtonTxt("Iniciar Sesión")
+            setWelcomeTxt("¡Bienvenido de vuelta!")
+            setAccountTxt("¿No tiene una cuenta?")
+            setSignupTxt("Regístrese Aquí")
+        } else {
+            setforgotPasswordTxt("Forgot Password?");
+            setLoginButtonTxt("Log In");
+            setWelcomeTxt("Welcome Back!");
+            setAccountTxt("Don't have an account?")
+            setSignupTxt("Sign Up for free")
+        }
+    }, [language]); 
+
+
+
+    const navigate = useNavigate();
 
     const handleSignUp = ()=> {
         navigate("/signup");
@@ -61,7 +86,7 @@ function LoginForm() {
     let modalElement = null;
     // if it true then create/render that component
     if (modalOpen) {
-        modalElement = <PasswordModal email={email} setEmail={setEmail} closePasswordModal={closePasswordModal} />;
+        modalElement = <PasswordModal email={email} setEmail={setEmail} closePasswordModal={closePasswordModal} language={language} />;
     }
 
     
@@ -70,18 +95,18 @@ function LoginForm() {
             <div className="LoginForm bg-white bg-center flex flex-col items-center w-100 h-140 rounded-md "> 
                 <div className="Title Header align-center mb-4">
                     <div className="Title mt-10 mb-4 text-center text-violet-600 font-bold text-5xl">Codifica</div>
-                    <div className="Caption mb-6 text-center text-black font-bold text-xl">Welcome Back!</div>
+                    <div className="Caption mb-6 text-center text-black font-bold text-xl">{WelcomeTxt}</div>
                 </div>
                 <form onSubmit={userLogin} className=" w-full flex flex-col items-center">
-                    <LoginInput username={username} setUsername={setUsername} password={password} setPassword={setPassword} ></LoginInput>
+                    <LoginInput username={username} setUsername={setUsername} password={password} setPassword={setPassword} language={language} ></LoginInput>
                     <div className= " w-2/3 flex flex-col items-center justify-center align-center">
-                        <p onClick = {openPasswordModal} className="mt-4 ml-1 text-center text-indigo-600 hover:text-purple-800 transition ease-in-out hover:cursor-pointer"> Forgot password?</p>
-                        <button type="submit" className="mt-2 mb-4 py-2 px-9 text-center font-semibold rounded-lg shadow-md text-white bg-indigo-700 hover:bg-blue-800 cursor-pointer"> Log In</button>
+                        <p onClick = {openPasswordModal} className="mt-4 ml-1 text-center text-indigo-600 hover:text-purple-800 transition ease-in-out hover:cursor-pointer"> {forgotPasswordTxt}</p>
+                        <button type="submit" className="mt-2 mb-4 py-2 px-9 text-center font-semibold rounded-lg shadow-md text-white bg-indigo-700 hover:bg-blue-800 cursor-pointer"> {loginButtonTxt}</button>
                     </div>
                 </form>
                 <div className= "flex flex-row">
-                    <p className="text-xs text-gray-500"> Don't have an account? </p>
-                    <p onClick={handleSignUp} className="ml-3 text-xs text-gray-500 underline hover:text-indigo-500 hover:cursor-pointer"> Sign up for free</p>
+                    <p className="text-xs text-gray-500"> {AccountTxt} </p>
+                    <p onClick={handleSignUp} className="ml-3 text-xs text-gray-500 underline hover:text-indigo-500 hover:cursor-pointer"> {signupTxt}</p>
                 </div>
                 {/* <button onClick={handleGoogleLogin} className="mt-4 px-4 py-2 font-semibold rounded-md shadow-md bg-red-300 text-white hover:bg-red-600 transition"> Sign in with Google </button> */}
             </div>
