3b317ddc9f1136624d61dfa0f994027efcbff321
Luimoe05 <lmoreno00528@gmail.com>
2025-08-01 10:26:12 -0700

    changed up some of the styling of home navbar

--- Frontend/src/Components/Pages/Home/HomeNavBar/HomeNavBar.jsx
+++ Frontend/src/Components/Pages/Home/HomeNavBar/HomeNavBar.jsx
@@ -339,9 +339,9 @@ export default function HomeNavBar({
                   </button>
                   <span>
                     <img
-                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png"
+                      src="https://adamchang.com/wp-content/uploads/2022/05/Tokyo-Olympics_Astro-Statics_large__Medal-Pose-1.png"
                       alt="pfp"
-                      className="w-10 cursor-pointer drop-shadow-lg rounded-full"
+                      className="w-20 cursor-pointer drop-shadow-lg rounded-full"
                       onClick={handleProfilePage}
                     />
                   </span>
@@ -374,7 +374,7 @@ export default function HomeNavBar({
                   <button onClick={onScrollToAboutUs}>About Us</button>
                   <button onClick={handleIDE}>Learn</button>
                   <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png"
+                    src="https://adamchang.com/wp-content/uploads/2022/05/Tokyo-Olympics_Astro-Statics_large__Medal-Pose-1.png"
                     alt="pfp"
                     className="w-10 rounded-full cursor-pointer"
                     onClick={handleProfilePage}
